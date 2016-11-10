4a8664ae484c436606021eab4f19bd5c44b71b79
Kit Fung <sa9510@gmail.com>
2016-11-10 14:18:23 +0800

    tmp

--- double_compare.cpp
+++ double_compare.cpp
@@ -215,8 +215,8 @@ Mat cutMiddle(Mat src) {
 		return result;*/
 }
 
-void new_compare(Mat src_color, int inputIndex, double the) {
-declare:
+vector<int> new_compare(Mat src_color, int inputIndex, double the) {
+
 		int debugIndex = -1;
 		//int inputIndex = -1;
 		int hbins_[3] = { 24, 24, 8 };
@@ -231,76 +231,19 @@ declare:
 		int method_count = METHOD_COUNT;
 
 		int hce_method_src = 0;
-		//int hce_method_dbimg = 0;
 
 		bool useHsv = true, useFea = true; // not ready 
-
-
 
 		string files[] = { "man", "beach", "building", "bus", "dinosaur", "elephant", "flower", "horse", "mountain", "food" };
 		int valid_indexs[] = { 0, 1, 2, 3, 4, 6, 7 }; // 0, 1, 2, 3, 4, 6, 7
 
-eachInput:
-		//for each (int valid_index in valid_indexs) {
-		//	inputIndex = valid_index;
-		// for DEBUG
-		//if (debugIndex != 1 && debugIndex != inputIndex)
-		// continue;
-
-start:
-		cout << endl << "double_compare:::::" << endl;
-		//vector<Mat> src_color_(8);
-		//vector<Mat> src_gray_(8);
-		//vector<Mat> src_color_middle_(8);
-		//vector<Mat> src_gray_middle_(8);
-
-eachInputSrc:
-
-
-
-		// 1: hc
-		// 2: e
-		// 3: hc>e
-		// 4: e>hc
-		//switch (hce_method_src) {
-		//		case 1:
-		//				src_color = toHigherContrast(src_color, higherContrast_src);
-		//				break;
-		//		case 3:
-		//				src_color = toHigherContrast(src_color, higherContrast_src);
-		//				src_color = im_so_awsome(src_color);
-		//				break;
-
-		//		case 2:
-		//				src_color = im_so_awsome(src_color);
-		//				break;
-		//		case 4:
-		//				src_color = toHigherContrast(src_color, higherContrast_src);
-		//				break;
-
-		//		default:
-		//				break;
-		//}
-
-
 		Mat src_gray; // imread(src.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
-		//src_gray = Mat(src_color,CV_L)
 		cv::cvtColor(src_color, src_gray, CV_BGR2GRAY); // CV_BGR2GRAY
 
 
 		Mat src_color_middle = cutMiddle(src_color);
 		Mat src_gray_middle = cutMiddle(src_gray);
 
-
-
-		//src_color_[valid_index] = src_color;
-
-		//src_gray_[valid_index] = src_gray;
-		//src_color_middle_[valid_index] = src_color_middle;
-		//src_gray_middle_[valid_index] = src_gray_middle;
-
-
-eachSetting:
 		vector<double> times_need;
 		vector<MethodResult> list_of_method_result;
 
@@ -543,14 +486,6 @@ eachSetting:
 		}
 		cout << endl;
 
-		//// compare hsv
-		//Mat src_hsv = rgbMat_to_hsvHist(src_input);
-
-		//for (int i = 0; i < features.size(); ++i)
-		//	imgScoreResult.push_back(ImgScore(i, compareHist(features[i], src_hsv, 0)));
-
-		//reportImgError(errorIndex);
-
 		ScoreReport sr;
 		sort(iss_list.rbegin(), iss_list.rend());
 
@@ -562,14 +497,10 @@ eachSetting:
 		}
 		sr = ScoreReport(iss_list, inputIndex);
 
-		// ===== sort method 1 // for hsv? , or (1 - fea)
-		//for each (int i in valid_indexs) {
-		//		for (int k = 0; k < iss_list_[i].size(); k++) {
-		//				sr = ScoreReport(iss_list_[i][k], i);
-		//				cout << "method#" << k + 1 << " : ";
-		//				bestMData_[i].update(sr, k + 1, hbins, sbins, minH);
-		//		}
-		//}
+		vector<int> res;
+		for (int i = 0; i < iss_list.size(); ++i)
+			res.push_back(iss_list[i].db_id);
+		return res;
 }
 
 
@@ -1098,252 +1029,3 @@ eachSetting:
 		//		bestMData_[vi].report();
 		//}
 }
-
-/*
-man		: 0 ~ 99
-beach	: 101 ~ 199
-building	: 200 ~ 299
-bus		: 300 ~ 399
-dinosaur	: 400 ~ 499
-elephant	: 500 ~ 599
-flower	: 600 ~ 699
-horse	: 700 ~ 799
-mountain	: 800 ~ 899
-food	: 900 ~ 999
-*/
-//void double_compare_bus() {
-//		// Mat src_input, vector<vector<Mat>> features, int inputIndex
-//		// Mat bus_1 = imread("./precut/bus_1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//		// Mat bus_1 = imread("./precut/bus_3.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//		// Mat bus_1 = imread("./bus.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//
-//		// imread("./precut/bus_1.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//		// Mat bus_2 = imread("./precut/bus_2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//
-//		vector<Mat> busPreFeature = {
-//				imread("./precut/bus_1.jpg", CV_LOAD_IMAGE_GRAYSCALE)
-//				, imread("./precut/bus_3.jpg", CV_LOAD_IMAGE_GRAYSCALE)
-//		};
-//
-//
-//		bool showImg = false;
-//		int start = 0, end = 1000;
-//		int minHessian;
-//
-//		double bestAcc = 0;
-//		int bestMinH = 0;
-//		int useCase = 0;
-//
-//		for (int j = 0; j < 100; j++) {
-//				minHessian = 4 + 4 * j;
-//
-//				SurfFeatureDetector detector(minHessian);
-//				SurfDescriptorExtractor extractor;
-//
-//				vector<std::vector<KeyPoint>> precut_keypoints(busPreFeature.size());
-//				vector<Mat> precut_descriptors(busPreFeature.size());
-//
-//				for (int i = 0; i < busPreFeature.size(); i++) {
-//						detector.detect(busPreFeature[i], precut_keypoints[i]);
-//						extractor.compute(busPreFeature[i], precut_keypoints[i], precut_descriptors[i]);
-//				}
-//
-//
-//				//std::vector<KeyPoint> keypoints_1, keypoints_2;
-//				//detector.detect(bus_1, keypoints_1);
-//
-//				std::vector<KeyPoint>keypoints_2;
-//
-//				Mat descriptors_dbimg;
-//
-//
-//
-//				vector<ImgScore> iss;
-//				vector<int> eIndex;
-//				for (int i = start; i < end; i++) {
-//						try {
-//								string file = "../image.orig/" + to_string(i) + ".jpg";
-//								Mat db_img = imread(file.c_str(), CV_LOAD_IMAGE_GRAYSCALE);
-//
-//								if (!db_img.data) {
-//										printf("Error loading images\n"); return;
-//								}
-//
-//								//vector<Mat> SURFDescriptors;
-//								//SURFDescriptors.push_back(calSURFDescriptor(bus_1));
-//								//SURFDescriptors.push_back(calSURFDescriptor(bus_2));
-//
-//								//-- Step 1: Detect the keypoints using SURF Detector
-//								detector.detect(db_img, keypoints_2);
-//
-//								//-- Step 2: Calculate descriptors (feature vectors)
-//								extractor.compute(db_img, keypoints_2, descriptors_dbimg);
-//
-//								//-- Step 3: Matching descriptor vectors with a brute force matcher
-//								BFMatcher matcher(NORM_L2);
-//								// BFMatcher matcher(NORM_HAMMING);
-//								// FlannBasedMatcher matcher;
-//
-//								std::vector<DMatch> matches;
-//								for (int i = 0; i < busPreFeature.size(); i++) {
-//										//matcher.match(descriptors_1, descriptors_dbimg, matches);
-//										matcher.match(precut_descriptors[i], descriptors_dbimg, matches);
-//								}
-//
-//
-//								//-- Draw matches
-//
-//								if (showImg) {
-//										Mat img_matches;
-//										for (int i = 0; i < busPreFeature.size(); i++) {
-//												drawMatches(busPreFeature[i], precut_keypoints[i], db_img, keypoints_2, matches, img_matches);
-//
-//												//drawMatches(bus_1, keypoints_1, db_img, keypoints_2, matches, img_matches);
-//												imshow("Matches", img_matches);
-//
-//												int keyValue = 0;
-//												while (keyValue >= 0) {
-//														keyValue = cvWaitKey(0);
-//
-//														switch (keyValue) {
-//																case 27:keyValue = -1;
-//																		break;
-//														}
-//												}
-//										}
-//								}
-//
-//
-//								//double score = feature_cmp(descriptors_1, descriptors_dbimg);
-//								double score = 0;
-//								for (int i = 0; i < busPreFeature.size(); i++) {
-//										score += feature_cmp(precut_descriptors[i], descriptors_dbimg);
-//								}
-//								score /= busPreFeature.size();
-//								iss.push_back(ImgScore(i, score));
-//								//printf(" img#%i, match score:%f \n", i, score);
-//								if (i % 10 == 0)
-//										printf(" %i%%..", i / 10);
-//								//-- Show detected matches
-//								// imshow("Matches", img_matches);
-//								// imshow("bus_1", bus_1);
-//						} catch (Exception e) {
-//								printf(" -----ERROR-----img#%i, %s\n", i, e.msg);
-//								eIndex.push_back(i);
-//						}
-//				}
-//				printf("\n\n");
-//
-//				// Sort
-//				sort(iss.begin(), iss.end());
-//
-//				reportImgError(eIndex);
-//				printf("-----\n");
-//
-//				ScoreReport sr(iss, 3);
-//				sr.reportSorted(300);
-//				sr.report();
-//
-//
-//				printf("=====End of minHessian=%3i\n=====\n", minHessian);
-//				//cout << "acc:" << sr.acc100 <<" , bestAcc" << bestAcc;
-//				if (sr.acc100 > bestAcc) {
-//						bestAcc = sr.acc100;
-//						bestMinH = minHessian;
-//				}
-//				printf("-Current best minH:%i, Acc100:%.0f", bestMinH, bestAcc);
-//		}// for j
-//		printf("best minH=%i Acc: %.4f%% | ", bestMinH, bestAcc);
-//
-//		printf("-1 to exit");
-//		int input;
-//		do {
-//				cin >> input;
-//		} while (input != -1);
-//
-//
-//}
-
-
-//
-//int bababa(int argc, char** argv) {
-//
-//	Mat img_1 = imread("./flower.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//	Mat img_2 = imread("../image.orig/685.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-//
-//	if (!img_1.data || !img_2.data) {
-//	printf("Error loading images\n"); return -1;
-//	}
-//
-//	//-- Step 1: Detect the keypoints using SURF Detector
-//	int minHessian = 400;
-//
-//	SurfFeatureDetector detector(minHessian);
-//
-//	std::vector<KeyPoint> keypoints_1, keypoints_2;
-//
-//	detector.detect(img_1, keypoints_1);
-//	detector.detect(img_2, keypoints_2);
-//
-//	//-- Step 2: Calculate descriptors (feature vectors)
-//	SurfDescriptorExtractor extractor;
-//
-//	Mat descriptors_1, descriptors_2;
-//
-//	extractor.compute(img_1, keypoints_1, descriptors_1);
-//	extractor.compute(img_2, keypoints_2, descriptors_2);
-//
-//	//-- Step 3: Matching descriptor vectors with a brute force matcher
-//	BFMatcher matcher(NORM_L2);
-//	std::vector< DMatch > matches;
-//	matcher.match(descriptors_1, descriptors_2, matches);
-//
-//	//-- Draw matches
-//	Mat img_matches;
-//	drawMatches(img_1, keypoints_1, img_2, keypoints_2, matches, img_matches);
-//
-//	//-- Show detected matches
-//	imshow("Matches", img_matches);
-//
-//	printf("Done \n");
-//	// Wait for the user to press a key in the GUI window.
-//	//Press ESC to quit
-//	int keyValue = 0;
-//	while (keyValue >= 0) {
-//	keyValue = cvWaitKey(0);
-//
-//	switch (keyValue) {
-//		case 27:keyValue = -1;
-//		break;
-//	}
-//	}
-//
-//	return 0;
-//}
-
-//
-//double hsv_split_compare(Mat src_input, vector<Mat> all_img, int inputIndex, bool output, int row, int col) {
-//	vector<Mat> imgs = all_img;
-//
-//	int tot = row*col;
-//	vector<Mat> src_hsv = rgbMat_to_divided_hsvHist(src_input, row, col);
-//
-//	vector<ImgScore> imgScoreResult;
-//
-//	for (int i = 0; i < imgs.size(); ++i) {
-//	vector<Mat> splited_img = rgbMat_to_divided_hsvHist(imgs[i], row, col);
-//	double scores = 0;
-//	double sums = 0;
-//	for (int i = 0; i < tot; ++i)
-//		sums += compareHist(splited_img[i], src_hsv[i], 0);
-//	imgScoreResult.push_back(ImgScore(i, sums / (double)(tot)));
-//	}
-//
-//	// sort
-//	sort(imgScoreResult.rbegin(), imgScoreResult.rend());
-//
-//	ScoreReport sr(imgScoreResult, inputIndex);
-//	sr.report(row, col);
-//
-//	return 0;
-//}
--- main.cpp
+++ main.cpp
@@ -45,224 +45,63 @@ string files[] = { "man", "beach", "building", "bus", "dinosaur", "elephant", "f
 int valid_indexs[] = { 0, 1, 2, 3, 4, 6, 7 };
 
 string getFilePath(int index) {
-		if (index < 0 || index >= 10)
-				index = 0;
-		string s = "./" + files[index] + ".jpg";
-		return s;
+	if (index < 0 || index >= 10)
+		index = 0;
+	string s = "./" + files[index] + ".jpg";
+	return s;
 }
 
 void file_select_instruction() {
-		cout << "Which file? (0:'man' 1:'beach', 2:'building', 3:'bus', 4:'dinosaur', " << endl
-				<< "            6:'flower', 7:'horse')? " << endl;
+	cout << "Which file? (0:'man' 1:'beach', 2:'building', 3:'bus', 4:'dinosaur', " << endl
+		<< "            6:'flower', 7:'horse')? " << endl;
 }
 
 
 Mat get_input_img(int index) {
-		string filepath = getFilePath(index);
+	string filepath = getFilePath(index);
 
-		Mat src_input = imread(filepath); // read input image
-		if (!src_input.data) {
-				printf("Cannot find the input image!\n");
-				exit(1);
-		}
-		return src_input;
+	Mat src_input = imread(filepath); // read input image
+	if (!src_input.data) {
+		printf("Cannot find the input image!\n");
+		exit(1);
+	}
+	return src_input;
 }
 
 void waitESC() {
-		int keyValue = 0;
-		while (keyValue >= 0) {
-				keyValue = cvWaitKey(0);
+	int keyValue = 0;
+	while (keyValue >= 0) {
+		keyValue = cvWaitKey(0);
 
-				switch (keyValue) {
-						case 27:keyValue = -1;
-								break;
-				}
+		switch (keyValue) {
+			case 27:keyValue = -1;
+				break;
 		}
+	}
 }
 
-double solve(int index) {
-		Mat src_input = get_input_img(index);
-		double acc = 0;
-
-		//imshow("Input", src_input);
-		// double acc = hsv_compare(src_input, index);
-		// double acc = hsv_split_compare(src_input, index);
-		// double acc = surf_compare(src_input, index);
-		// double acc = sift_compare(src_input, index);
-		// double acc = orb_compare(src_input, index);
-		// double acc = psnr_compare(src_input, index);
-		// double acc = mssim_compare(src_input, index);
-
-		// combin_compare_6
-		// double acc = combin_compare_16(src_input, index, 1.2);
-
-		return acc;
+void solve(int index) {
+	Mat src_input = get_input_img(index);
+	vector<int> res = new_compare(src_input, index, 6.619589);
+	save_result(res);
 }
 
-void test_combin() {
-		//double besti = -1, maxac = 0;
+void extra_fucntion() {
 
-		//int count = 0;
-		//MethodResult best_mr;
-		//for (double con_setting = 1.15; con_setting <= 1.25; con_setting += 0.05) {
-		//		printf("\n -- i = %lf -- \n", con_setting);
-
-		//		vector<double> acc_;
-
-		//		for (int i = 0; i < 7; i++) {
-		//				acc_.push_back(0);
-		//		}
-
-		//		MethodResult mr;
-		//		mr.add_double_setting(con_setting);
-
-		//		// for each input
-		//		for (auto index : valid_indexs) {
-		//				Mat src_input = get_input_img(index);
-		//				//acc += combin_compare_15(src_input, index, i);
-		//				if (count == 0) {
-		//						//acc_[0] += combin_compare_1(src_input, index);
-		//						//acc_[1] += combin_compare_2(src_input, index);
-		//						//acc_[2] += combin_compare_3(src_input, index);
-		//						//acc_[3] += combin_compare_4(src_input, index);
-		//				} else {
-		//						acc_[0] += 0;
-		//						acc_[1] += 0;
-		//						acc_[2] += 0;
-		//						acc_[3] += 0;
-		//				}
-
-		//				acc_[4] += combin_compare_5(src_input, index, con_setting);
-		//				acc_[5] += 0;
-		//				//acc_[6] += combin_compare_7(src_input, index, con_setting);
-
-
-		//		}
-		//		for (int i = 0; i < 7; i++) {
-		//				acc_[i] /= 7;
-		//				mr.add_acc(acc_[i]);
-		//		}
-
-		//		double mr_best_acc = mr.getBestAcc();
-		//		if (mr_best_acc > maxac) {
-		//				maxac = mr_best_acc;
-		//				besti = con_setting;
-		//				best_mr = mr;
-		//		}
-		//		printf("\n so far: Best acc: %lf || i = %lf\n", maxac, besti);
-		//		best_mr.report();
-		//}
-		//printf("\nBest acc: %lf || i = %lf\n", maxac, besti);
-		//best_mr.report();
-}
-
-void test_all_average() {
-		double cnt = 7;
-		double totacc = 0;
-		vector<double> accs;
-		for (auto idx : valid_indexs) {
-				double acc = solve(idx);
-				totacc += acc;
-				accs.push_back(acc);
-		}
-
-		for (int i = 0; i < accs.size(); ++i) {
-				int idx = valid_indexs[i];
-				double acc = accs[i];
-				cout << "Case " << idx << " acc: " << acc << endl;
-		}
-		cout << "Total average acc: " << totacc / cnt << endl;
-}
-
-void test_r1c1_to_r10c10() {
-		for (auto v_index : valid_indexs) {
-				Mat src_input = get_input_img(v_index);
-
-				printf("Work for img \"%s\" :\n", files[v_index].c_str());
-				for (int i = 0; i < 11; i++)
-						for (int j = 0; j < 11; j++)
-								hsv_split_compare(src_input, v_index, false, i + 1, j + 1);
-
-				printf("Done for img \"%s\".\n", files[v_index].c_str());
-
-		}
-}
-
-void test_all_hsv_compare() {
-		for (auto v_index : valid_indexs) {
-				Mat src_input = get_input_img(v_index);
-
-				printf("Work for img \"%s\" :\n", files[v_index].c_str());
-				hsv_compare(src_input, v_index);
-				printf("Done for img \"%s\".\n", files[v_index].c_str());
-
-		}
 }
 
 
 int main(int argc, char** argv) {
-		Mat src_input;
+	Mat src_input;
 
-		Mat max_img;
+	Mat max_img;
 
-		file_select_instruction();
-		cout << " or -1 for all average: " << endl;
-		cout << " or -2 for preprocessing(): " << endl;
-		cout << " or -3 for test from r1c1 to r10c10  : " << endl;
-		cout << " or -4 for test all hsv_compare()    : " << endl;
-		cout << " or -5 for double_compare()          : " << endl;
-		cout << " or -6 for test_combin()             : " << endl;
+	file_select_instruction();
+	int instruction;
+	cin >> instruction;
+	solve(instruction);
 
-		int index;
-		cin >> index;
-
-		if (index == -2) {
-				bool greyscale;
-				cout << "0 not greyScale, 1 greyScale: \n";
-				cin >> greyscale;
-
-				int type;
-				cout << " 0 for sift , 1 for surf, 2 for orb, 3 for sift bow, 4 for surf bow, 5 for orb bow: " << endl;
-				cin >> type;
-				if (type == 0)
-						save_allSIFTDescriptions_YML(greyscale);
-				else if (type == 1)
-						save_allSURFDescriptions_YML(greyscale);
-				else if (type == 2)
-						save_allORBDescriptions_YML(greyscale);
-				else if (type == 3)
-						save_BOW("SIFT", greyscale);
-				else if (type == 4)
-						save_BOW("SURF", greyscale);
-				else if (type == 5)
-						save_BOW("ORB", greyscale);
-
-				waitESC();
-				return 0;
-		} else if (index == -1) {
-				test_all_average();
-		} else if (index == -3) {
-				test_r1c1_to_r10c10();
-		} else if (index == -4) {
-				test_all_hsv_compare();
-		} else if (index == -5) {
-				double_compare();
-		} else if (index == -6) {
-				test_combin();
-		} else if (index == -7){
-
-		} else {
-				solve(index);
-				vector<int> res = { 1, 23, 456, 1345 };
-				save_result(res);
-		}
-
-		do {
-				cout << "input -1 for exit...\n";
-				cin >> index;
-		} while (index != -1);
-
-		waitESC();
-
-		return 0;
+	cout << "Done!\n";
+	waitESC();
+	return 0;
 }
